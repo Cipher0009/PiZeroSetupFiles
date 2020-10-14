a73874dd3753dfc7c469b3adefd41ca7ac871c36
Cipher0009 <46619339+Cipher0009@users.noreply.github.com>
2020-10-13 22:27:19 -0400

    Setup PiZero Pimoroni 240x240 screen

---
+++ PiZeroScreen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+##### Prior to running make sure config is correct compare to github /boot/config.txt
+##### For usbotg after flashing sdcard, ensure empty ssh is created and commandline.txt is correct prior to starting pi 
+##### Immediately after startup run raspi-config, enable spi i2c ssh, and CHANGE LOGIN CREDENTIALS 
+sudo apt update && sudo apt upgrade 
+sudo apt install git mc fail2ban python-rpi.gpio python-spidev python-pip python-pil python-numpy python3 python3-rpi.gpio python3-spidev python3-pip python3-pil python3-numpy --no-install-recommends
+git clone https://github.com/pimoroni/st7789-python.git
+pip install st7789
+sudo apt update && sudo apt upgrade
+#####To test screen functionality
+#cd /st7789-python/examples/
+#python image.py cat.jpg
