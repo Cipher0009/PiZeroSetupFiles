63e24fbcdc7513b5a41367700bc6882a39a9d2a3
Cipher0009 <46619339+Cipher0009@users.noreply.github.com>
2020-10-15 20:39:01 -0400

    Update PiZeroScreen.sh
    Cleaned up install and added htop to watch system a bit better

--- PiZeroScreen.sh
+++ PiZeroScreen.sh
@@ -3,7 +3,7 @@
 ##### For usbotg after flashing sdcard, ensure empty ssh is created and commandline.txt is correct prior to starting pi 
 ##### Immediately after startup run raspi-config, enable spi i2c ssh, and CHANGE LOGIN CREDENTIALS 
 sudo apt update && sudo apt upgrade 
-sudo apt install git mc fail2ban python-rpi.gpio python-spidev python-pip python-pil python-numpy python3 python3-rpi.gpio python3-spidev python3-pip python3-pil python3-numpy --no-install-recommends
+sudo apt install git mc htop fail2ban python3 python3-rpi.gpio python3-spidev python3-pip python3-pil python3-numpy --no-install-recommends
 git clone https://github.com/pimoroni/st7789-python.git
 pip install st7789
 sudo apt update && sudo apt upgrade
